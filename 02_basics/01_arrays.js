664ed0338498bf4e686bbc83d792904ab44d7d20
div174 <155170554+div174@users.noreply.github.com>
2025-04-08 03:15:53 +0000

    2 in mid

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,7 +1,8 @@
 // array
 
+// heterogenous, resizable, shallow copy
 const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+const myHeroes = ["shaktiman", "naagraj"]
 
 const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr[1]);
@@ -12,28 +13,29 @@ const myArr2 = new Array(1, 2, 3, 4)
 // myArr.push(7)
 // myArr.pop()
 
-// myArr.unshift(9)
-// myArr.shift()
+// myArr.unshift(9)//add element in starting of array
+// myArr.shift()//remove starting element
 
-// console.log(myArr.includes(9));
+// console.log(myArr.includes(9));//false
+// console.log(myArr.indexOf(9));//-1
 // console.log(myArr.indexOf(3));
 
-// const newArr = myArr.join()
+const newArr = myArr.join()
 
 // console.log(myArr);
-// console.log( newArr);
+// console.log( newArr);//type - string
 
 
 // slice, splice
 
 console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(1, 3)//returns copy of section[(a,b)-frm a to b-1] of array
 
 console.log(myn1);
 console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
+const myn2 = myArr.splice(1, 3)//remove and return the section[(a,b)-frm a to b] of array,, manipulates the original array
 console.log("C ", myArr);
 console.log(myn2);
--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,30 +1,47 @@
+//obj ko 2 ways s bna skte h
+// 1- using contructor---jo obj bnega vo singleton hga(apne trh ka only one obj)
+//2-using obj literals---not singleton(multiple instances bn skte h iske)
+
+
+// obj m key, val define hti h
+
 // singleton
-// Object.create
+// Object.create//constructor method
 
 // object literals
 
-const mySym = Symbol("key1")
+const mySym = Symbol("key0")
+const mySym1 = Symbol("key1")
 
 
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    //symbol as key
+    [mySym]: "mykey0",//using symbol as symbol
+    mySym1:"mykey1",//symbol as string
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
-}
+}//here name is treated as "name"
 
 // console.log(JsUser.email)
 // console.log(JsUser["email"])
+// console.log(JsUser."full name")//error
 // console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser.mySym)
+// console.log(JsUser[mySym])
+// console.log(JsUser.mySym1)
+// console.log(JsUser[mySym1])
+
+// console.table([typeof JsUser.mySym, typeof JsUser[mySym], typeof JsUser.mySym1, typeof JsUser[mySym1] ])
+
+// JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)//now we cant change this obj
+// JsUser.email = "hitesh@microsoft.com"//change nhi hga, error bhi nhi dega
 // console.log(JsUser);
 
 JsUser.greeting = function(){
@@ -34,5 +51,6 @@ JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting);//[Function (anonymous)]---fn execute nhi hua h, bs reference return hua h
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
