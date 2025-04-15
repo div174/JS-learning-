2ed314ca08b4fdedb1d63cbc52e10c6b139afae1
div174 <155170554+div174@users.noreply.github.com>
2025-04-15 02:16:34 +0000

    3-1 mid

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -24,10 +24,11 @@ const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
 // const obj3 = { obj1, obj2 }
-// const obj3 = Object.assign({}, obj1, obj2, obj4)
+// const obj3 = Object.assign({}, obj1, obj2, obj4)//all elements of all objects written in assign method go in the first object hence we used {} as first obj -it is the target, and all other objs r sources
 
 const obj3 = {...obj1, ...obj2}
 // console.log(obj3);
+// console.log(obj1);
 
 
 const users = [
@@ -37,22 +38,23 @@ const users = [
     },
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "i@gmail.com"
     },
     {
         id: 1,
-        email: "h@gmail.com"
+        email: "j@gmail.com"
     },
-]
+] // array of objects.
 
-users[1].email
+// console.log(users[1].email);
+
 // console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
-// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));//to chk if a property or key is existing in the obj
 
 
 const course = {
@@ -61,12 +63,15 @@ const course = {
     courseInstructor: "hitesh"
 }
 
+// obj destructuring---
 // course.courseInstructor
-
-const {courseInstructor: instructor} = course
-
+const {courseInstructor} = course
 // console.log(courseInstructor);
-console.log(instructor);
+const {courseInstructor: instructor} = course
+// console.log(courseInstructor);
+// console.log(instructor);
+
+// APIs in json format-
 
 // {
 //     "name": "hitesh",
@@ -74,9 +79,9 @@ console.log(instructor);
 //     "price": "free"
 // }
 
-[
-    {},
-    {},
-    {}
-]
+// [
+//     {},
+//     {},
+//     {}
+// ]
 
--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,44 +6,52 @@ function sayMyName(){
     console.log("E");
     console.log("S");
     console.log("H");
-}
+}//fn def
+// sayMyName // fn reference
+// sayMyName()//fn execution
 
-// sayMyName()
-
-// function addTwoNumbers(number1, number2){
+// function addTwoNumbers(number1, number2){//no datatype given when giving parameters
 
 //     console.log(number1 + number2);
 // }
+
+// addTwoNumbers() //NaN--no arguments given
+// addTwoNumbers(3,"4")//34
+// const result = addTwoNumbers(3, 5)
+// console.log("Result: ", result);//undefined  : fn is nt returning, bt printing in the console
 
 function addTwoNumbers(number1, number2){
 
     // let result = number1 + number2
     // return result
     return number1 + number2
+    console.log("div");//unreachable statement: return ke bd ki statement execute nhi hoti
 }
 
-const result = addTwoNumbers(3, 5)
+// const result = addTwoNumbers(3, 5)
 
 // console.log("Result: ", result);
 
 
 function loginUserMessage(username = "sam"){
-    if(!username){
+    // if(username===undefined){
+    if(!username){//if no arg is passed and parameter m =sam nhi likha, thn username undefined hoga"
         console.log("PLease enter a username");
         return
     }
-    return `${username} just logged in`
+    // return "${username} just logged in"
+    return `${username} just logged in`//string interpolation
 }
 
-// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage())
 // console.log(loginUserMessage("hitesh"))
 
-
+// ... - rest operator- used when num of args r unknown
 function calculateCartPrice(val1, val2, ...num1){
     return num1
 }
 
-// console.log(calculateCartPrice(200, 400, 500, 2000))
+// console.log(calculateCartPrice(200, 400, 500, 2000))//returns arr : [ 500, 2000 ]
 
 const user = {
     username: "hitesh",
@@ -55,10 +63,11 @@ function handleObject(anyobject){
 }
 
 // handleObject(user)
-handleObject({
-    username: "sam",
-    price: 399
-})
+
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
 
 const myNewArray = [200, 400, 100, 600]
 
@@ -66,5 +75,6 @@ function returnSecondValue(getArray){
     return getArray[1]
 }
 
+returnSecondValue(myNewArray)//blank output
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+// console.log(returnSecondValue([200, 400, 500, 1000]));
